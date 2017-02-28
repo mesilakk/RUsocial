3c5f98d68e093b87f28fee829c59261bf69c2b30
Dmilton <dmilton3@radford.edu>
2017-02-28 17:23:22 -0500

    TeamTutorial
    Just to practice merging, conflicts, and additions

---
+++ GitTest/src/GitTest.java
@@ -0,0 +1,25 @@
+/**
+ * Created by DMan on 2/28/2017.
+ */
+public class GitTest {
+
+    public static void main(String[] args){
+
+        char[] pack = {'R','A','D','P', 'A','C','K'};
+
+        System.out.println("This is a Test");
+
+        int check = 10;
+        do{
+            System.out.println(check);
+            check--;
+        }while(check != 0);
+
+        System.out.println("We are the..");
+
+        for(int pos = 0; pos < pack.length; pos++){
+            System.out.println(pack[pos]);
+        }
+
+    }
+}
